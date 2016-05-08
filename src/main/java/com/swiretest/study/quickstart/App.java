ca1e55b3f12dab98483cfd15a32e312245bdd924
guyuefeng <guyuefengxue@gmail.com>
2016-05-08 20:30:19 +0800

    dfsdf

--- src/main/java/com/swiretest/study/quickstart/App.java
+++ src/main/java/com/swiretest/study/quickstart/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println("test git hub");
+        
+        System.out.println("test conflict");
     }
 }
