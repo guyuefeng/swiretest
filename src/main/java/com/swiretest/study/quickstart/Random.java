d8c0fa71147a3cae467ab0ddab64411b240a5f82
guyuefeng <guyuefengxue@gmail.com>
2016-05-22 20:50:30 +0800

    test3

--- src/main/java/com/swiretest/study/quickstart/Random.java
+++ src/main/java/com/swiretest/study/quickstart/Random.java
@@ -4,11 +4,12 @@ public class Random {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("test Reandom");
-		
+
 		System.out.println("test2");
 
+		System.out.println("test3");
 	}
 
 }
